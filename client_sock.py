4b2c547e59691dfcbc45508270416163ab45e8f9
abdelaliLaatamli <abdelaliLaatamli@gmail.com>
2023-01-15 22:26:19 +0100

    first commit

---
+++ client_sock.py
@@ -0,0 +1,80 @@
+import socket
+import ssl
+import os
+import base64
+
+certchain = f"{os.getcwd()}{os.sep}keys{os.sep}certchain.pem"
+private = f"{os.getcwd()}{os.sep}keys{os.sep}private.key"
+
+HOST = "smtp.gmail.com"  # The server's hostname or IP address
+PORT = 587  # The port used by the server
+# PORT = 25  # The port used by the server
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    #s.set
+    s.bind(("51.222.146.133",59166))
+    s.settimeout(10)
+    s.connect((HOST, PORT))
+    data = s.recv(1024)
+    print(data.decode())
+
+    # s.send(b"EHLO smtp.gmail.com\r\n")
+    s.send(b"EHLO smtp.test.com\r\n")
+    data = s.recv(1024)
+    print(data.decode())
+
+    s.send(b"starttls\r\n")
+
+    data = s.recv(1024)
+    print(data.decode())
+
+    s = ssl.wrap_socket(s, ssl_version=ssl.PROTOCOL_SSLv23)
+
+    s.send(b"AUTH LOGIN\r\n")
+    data = s.recv(1024)
+    print(data.decode())
+
+    s.send( f"{base64.b64encode(b'abdelalilaatamli@gmail.com').decode()}\r\n".encode() )
+    data = s.recv(1024)
+    print(data.decode())
+
+
+    s.send( f"{base64.b64encode(b'nakckpcfpsqsvrzc').decode()}\r\n".encode() )
+    data = s.recv(1024)
+    print(data.decode())
+
+
+    
+    s.send( "MAIL FROM: <abdelalilaatamli@gmail.com>\r\n".encode() )
+    data = s.recv(1024)
+    print(data.decode())
+
+    s.send( "RCPT TO: <abdo332015@gmail.com>\r\n".encode() )
+    data = s.recv(1024)
+    print(data.decode())
+
+    s.send( "DATA\r\n".encode() )
+    data = s.recv(1024)
+    print(data.decode())
+    
+
+    s.send( b"Subject: test\r\n")
+    s.send( b"This is a line in the email.\nThis is a second line in the email." )
+    s.send( b"\r\n.\r\n")
+    #s.send( b"Subject: test\r\nThis is a line in the email.\nThis is a second line in the email.\r\n.\r\n" )
+    data = s.recv(1024)
+    print(data.decode())
+
+
+    s.send( b"QUIT\r\n" )
+    data = s.recv(1024)
+    print(data.decode())
+
+
+    # quit rset
+
+    # print("Message after connection request:" + recv)
+
+#print(f"Received {data!r}")
---
+++ readme.md
@@ -0,0 +1,4 @@
+- [Sending mail using SMTP protocol](https://stdin.top/posts/smtp-sendmail/)
+- [Build a TCP proxy in Python (part 1/3)](https://medium.com/@gdieu/build-a-tcp-proxy-in-python-part-1-3-7552cd5afdfe)
+- [ssl — TLS/SSL wrapper for socket objects](https://www.docs4dev.com/docs/en/python/3.7.2rc1/all/library-ssl.html)
+- [Python réseau socket](https://python.doctor/page-reseaux-sockets-python-port)
